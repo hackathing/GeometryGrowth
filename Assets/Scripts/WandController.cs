f791035640858f9b0736d01a021b03df9f8d77fe
Robbie Coomber <robbie.coomber@gmail.com>
2016-06-26 18:40:43 +0100

    Add WandController script

---
+++ Assets/Scripts/WandController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+    public class WandController : MonoBehaviour
+    {
+        public GameObject PrefabToSpawn;
+
+        private SteamVR_Controller.Device Controller { get { return SteamVR_Controller.Input((int)_trackedObj.index); } }
+        private SteamVR_TrackedObject _trackedObj;
+
+        // Use this for initialization
+        void Start()
+        {
+            _trackedObj = GetComponent<SteamVR_TrackedObject>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Controller == null)
+            {
+                return;
+            }
+
+            var gripButtonPress = Controller.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger);
+
+            if (gripButtonPress)
+            {
+                Debug.Log("Press");
+                GrowTree(Controller.transform.pos);
+            }
+        }
+
+        void GrowTree(Vector3 position)
+        {
+            Instantiate(PrefabToSpawn, position, Quaternion.identity);
+        }
+    }
+}
+
+
