0901f3e97bf318d674d6efa963b82b92de2267fd
Robbie Coomber <robbie.coomber@gmail.com>
2016-06-26 17:06:03 +0100

    Initial commit

---
+++ Assets/Fractal.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fractal : MonoBehaviour {
+
+	public Mesh mesh;
+	public Material material;
+	public int maxDepth;
+	public float childScale;
+	public float growthFrameTime;
+
+	private int depth;
+
+	private static Vector3[] childDirections = {
+		Vector3.up,
+		Vector3.right,
+		Vector3.left
+	};
+
+	private static Quaternion[] childOrientations = {
+		Quaternion.identity,
+		Quaternion.Euler(0f, 0f, -90f),
+		Quaternion.Euler(0f, 0f, 90f)
+	};
+
+	// Use this for initialization
+	void Start () {
+		gameObject.AddComponent<MeshFilter>().mesh = mesh;
+		gameObject.AddComponent<MeshRenderer>().material = material;
+		if (depth < maxDepth) {
+			StartCoroutine(CreateChildren());
+		}
+	}
+
+	private IEnumerator CreateChildren () {
+		for (int i = 0; i < childDirections.Length; i++) {
+            yield return new WaitForSeconds(growthFrameTime);
+            new GameObject ("Fractal Child").AddComponent<Fractal> ().Initialize (this, i);
+		}
+	}
+
+	private void Initialize (Fractal parent, int childIndex) {
+		mesh = parent.mesh;
+		material = parent.material;
+		maxDepth = parent.maxDepth;
+		depth = parent.depth + 1;
+		childScale = parent.childScale;
+	    growthFrameTime = parent.growthFrameTime;
+		transform.parent = parent.transform;
+		transform.localScale = Vector3.one * childScale;
+		transform.localPosition = childDirections[childIndex] * (0.5f + 0.5f * childScale);
+		transform.localRotation = childOrientations[childIndex];
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+}
