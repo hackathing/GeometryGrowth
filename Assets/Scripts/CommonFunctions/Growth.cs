a83f289aeda7d7e66701fd940c3afa6fadf88b17
admin <admin@admins-mbp.home>
2016-12-03 19:34:50 +0000

    Failed attempt to control tree growth speed
    	Added a Grwoth class with a NewPosition function to be used instead of Lerp to grow something over a fixed duration.
    This should work in principle but my initial attempt faild because the growth rate parameter was not being correctly passed to objects created using coroutines.  I was probably just going about it the wrong way but am tempted to re-write all of the fractal code in a more functional style.

---
+++ Assets/Scripts/CommonFunctions/Growth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Growth {
+
+  public static Vector3 NewPosition (Vector3 current, Vector3 initial, Vector3 final, float duration) {
+    float speed = (final - initial).magnitude / duration;
+    Debug.Log("-----1");
+    Debug.Log(final);
+    Debug.Log(initial);
+    Debug.Log(duration);
+    Debug.Log((final - initial).magnitude);
+    
+    Vector3 toEnd = final - current;
+    Vector3 move = toEnd.normalized * speed * Time.deltaTime;
+    Debug.Log("------");
+    Debug.Log(speed);
+    if (toEnd.sqrMagnitude < move.sqrMagnitude) { // Check if end is reached
+      Debug.Log(final);
+      return final;
+    } else {
+      Debug.Log(current + move);
+      return (current + move);
+    }
+  }
+
+}
--- Assets/Scripts/FractalPlants/Oleg/OlegTree.cs
+++ Assets/Scripts/FractalPlants/Oleg/OlegTree.cs
@@ -3,87 +3,112 @@ using System.Collections;
 
 public class OlegTree : MonoBehaviour {
 
-	public Mesh mesh;
-	public Material material;
-	public int maxDepth;
-	public float childScale;
-	public float growthFrameTime;
-	public float spawnProbability;
+  public Mesh mesh;
+  public Material material;
+  public int maxDepth;
+  public float childScale;
+  public float growthFrameTime;
+  public float spawnProbability;
+  public float growthDuration;  // The growthDuration variable is not working as intended here
 
-	private int depth;
+  private int depth;
 
-	private static Vector3[] childDirections = {
-		Vector3.up,
-		Vector3.right,
-		Vector3.left
-	};
+  private static Vector3[] childDirections = {
+    Vector3.up,
+    Vector3.right,
+    Vector3.left
+  };
 
-	private static Quaternion[] childOrientations = {
-		Quaternion.identity,
-		Quaternion.Euler(0f, 0f, -60f),
-		Quaternion.Euler(0f, 0f, 60f)
-	};
+  private static Quaternion[] childOrientations = {
+    Quaternion.identity,
+    Quaternion.Euler(0f, 0f, -60f),
+    Quaternion.Euler(0f, 0f, 60f)
+  };
 
-	// This is a bit weird. This is to compensate for the parent scale
-	private static Vector3[] childScales = {
-		new Vector3(1f, 1f, 1f),
-		new Vector3(0.5f, 2f, 1f),
-		new Vector3(0.5f, 2f, 1f)
-	};
+  // This is a bit weird. This is to compensate for the parent scale
+  private static Vector3[] childScales = {
+    new Vector3(1f, 1f, 1f),
+    new Vector3(0.5f, 2f, 1f),
+    new Vector3(0.5f, 2f, 1f)
+  };
 
-	// Use this for initialization
-	void Start () {
-		var meshFilter = gameObject.AddComponent<MeshFilter>();
-	    meshFilter.mesh = mesh;
-	    var meshRenderer = gameObject.AddComponent<MeshRenderer>();
+  // Use this for initialization
+  void Start () {
+    var meshFilter = gameObject.AddComponent<MeshFilter>();
+      meshFilter.mesh = mesh;
+      var meshRenderer = gameObject.AddComponent<MeshRenderer>();
         meshRenderer.material = material;
-		if (depth < maxDepth) {
-			StartCoroutine(CreateChildren());
-		}
-	}
+    if (depth < maxDepth) {
+      StartCoroutine(CreateChildren());
+    }
 
-	private IEnumerator CreateChildren () {
-		for (int i = 0; i < childDirections.Length; i++) {
-			if (Random.value < spawnProbability) {
-				yield return new WaitForSeconds (growthFrameTime);
-				new GameObject ("OlegTree Child").AddComponent<OlegTree> ().Initialize (this, i);
-			}
-		}
-	}
+    // Debug.Log("growthDuration in Start");
+    // Debug.Log(this);
+    // Debug.Log(growthDuration);
 
-	private void Initialize (OlegTree parent, int childIndex) {
-		spawnProbability = parent.spawnProbability;
-		mesh = parent.mesh;
-		material = parent.material;
-		maxDepth = parent.maxDepth;
-		growthFrameTime = parent.growthFrameTime;
-		depth = parent.depth + 1;
-		childScale = parent.childScale;
-		transform.parent = parent.transform;
-		transform.localPosition = childDirections[childIndex] * 0.5f;
-		transform.localRotation = childOrientations[childIndex];
-		StartCoroutine(scaleObject(childIndex));
-	}
+  }
 
-	private IEnumerator scaleObject(int childIndex) {
-		this.transform.localScale = Vector3.one * 0;
-		Vector3 destinationScale = (childScales[childIndex] * childScale);
+  private IEnumerator CreateChildren () {
+    for (int i = 0; i < childDirections.Length; i++) {
+      if (Random.value < spawnProbability) {
+        yield return new WaitForSeconds (growthFrameTime);
 
-		Vector3 destinationPosition = childDirections[childIndex] * (0.5f + 0.5f * childScale);
-		float time = 10.0f;
-		float currentTime = 0.0f;
+        new GameObject ("OlegTree Child").AddComponent<OlegTree> ().Initialize (this, growthDuration, i);
+      }
+    }
+  }
 
-		do
-		{
-			this.transform.localScale = Vector3.Lerp(this.transform.localScale, destinationScale, currentTime / time);
-			this.transform.localPosition = Vector3.Lerp(this.transform.localPosition, destinationPosition, currentTime / time);
-			currentTime += Time.deltaTime;
-			yield return null;
-		} while (currentTime <= time);
-	}
+  private void Initialize (OlegTree parent, float growthDuration, int childIndex) {
+    spawnProbability = parent.spawnProbability;
+    mesh = parent.mesh;
+    material = parent.material;
+    maxDepth = parent.maxDepth;
+    growthFrameTime = parent.growthFrameTime;
+    depth = parent.depth + 1;
+    childScale = parent.childScale;
+    transform.parent = parent.transform;
+    transform.localPosition = childDirections[childIndex] * 0.5f;
+    transform.localRotation = childOrientations[childIndex];
+    // Debug.Log("growthDuration in Initialize");
+    // Debug.Log(growthDuration);
+    StartCoroutine(scaleObject(childIndex, growthDuration));
+  }
 
-	// Update is called once per frame
-	void Update () {
+  private IEnumerator scaleObject(int childIndex, float growthDuration) {
 
-	}
+    Vector3 initialScale = Vector3.one * 0;
+    // Vector3 initialPosition = this.transform.localPosition;
+    
+    this.transform.localScale = initialScale;
+    Vector3 destinationScale = (childScales[childIndex] * childScale);
+    Vector3 destinationPosition = childDirections[childIndex] * (0.5f + 0.5f * childScale);
+
+    // Debug.Log(this.transform.localScale);
+    // Debug.Log(this.transform.localPosition);
+
+    float time = 10.0f;
+    float currentTime = 0.0f;
+    
+    do
+    {
+      // this.transform.localScale = Vector3.Lerp(this.transform.localScale, destinationScale, currentTime / time);
+      // Debug.Log("growthDuration in scaleObject,");
+      // Debug.Log(childIndex);
+      // Debug.Log(growthDuration);
+      // this.transform.localScale = Growth.NewPosition(this.transform.localScale, initialScale, destinationScale, growthDuration);
+      // this.transform.localPosition = Growth.NewPosition(this.transform.localPosition, initialPosition, destinationPosition, growthDuration);
+
+      // The growthDuration variable is not working as intended here
+      this.transform.localScale = Vector3.Lerp(this.transform.localScale, destinationScale, currentTime / time);
+      this.transform.localPosition = Vector3.Lerp(this.transform.localPosition, destinationPosition, currentTime / time);
+      
+      currentTime += Time.deltaTime;
+      yield return null;
+    } while (currentTime <= time);
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
 }
