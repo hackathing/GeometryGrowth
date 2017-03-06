e568636f6d401a0a89a866216ad66e15ca1c1a26
Robbie Coomber <robbie.coomber@gmail.com>
2017-03-06 11:12:16 +0000

    Add selectable script that was missed in last commit

---
+++ Assets/Scripts/Selectable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Selectable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetHighlight(bool shouldHighlight)
+    {
+        var components = GetComponentsInChildren<MeshRenderer>(includeInactive: true);
+
+        foreach (var component in components)
+        {
+            if (component.gameObject.name == "Highlight")
+            {
+                var outlineRenderer = component.GetComponent<MeshRenderer>();
+                outlineRenderer.enabled = shouldHighlight;
+            }
+        }
+    }
+}
